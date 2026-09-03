06b6514538054b21dcddd86668ba35872016280b
biscuitbc <938605856@qq.com>
2026-09-03 19:29:04 +0800

    ass04 part1

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -12,12 +12,31 @@ std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
   /* TODO: Implement this method */
-  return Corpus();
+  // Step 1
+  auto spaces = find_all(source.begin(), source.end(), ::isspace);
+  // Step 2
+  Corpus Tok;
+
+  std::transform(spaces.begin(), spaces.end() - 1, spaces.begin() + 1, std::inserter(Tok, Tok.end()), 
+            [&source](auto it1, auto it2){
+              return Token(source, it1, it2);
+            });
+  // Step 3
+  std::erase_if(Tok, [](const Token& token) {
+    return token.content.empty();
+  });
+
+  
+
+  return Tok;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
   /* TODO: Implement this method */
-  return std::set<Misspelling>();
+  
+  
+
+
 };
 
 /* Helper methods */
